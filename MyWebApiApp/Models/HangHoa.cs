b9e32108604ab4ae606e17824d00635b9717db7a
Norman <khanhuchiha2k4@gmail.com>
2024-05-20 14:55:20 +0700

    Started to building first API Web project based on swagger and postman

---
+++ MyWebApiApp/Models/HangHoa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyWebApiApp.Models
+{
+    public class HangHoaVM
+    {
+        public string TenHangHoa {  get; set; }
+        public string DonGia {  get; set; }
+    }public class HangHoa : HangHoaVM
+    {
+        public Guid MaHangHoa { get; set;}
+    }
+}
